b484f32d550fdd1014a8f774679bd6163acbd91b
deepshukla <deepshukla0901@gmail.com>
2024-12-01 15:36:28 +0100

    first commit

---
+++ build/MassSpringSystem/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/deepshukla/aopt-exercise9_JulesOdje/aopt-exercise9_JulesOdje/aopt-exercise9/aopt-exercise9/MassSpringSystem
+# Build directory: /home/deepshukla/aopt-exercise9_JulesOdje/aopt-exercise9_JulesOdje/aopt-exercise9/aopt-exercise9/build/MassSpringSystem
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
